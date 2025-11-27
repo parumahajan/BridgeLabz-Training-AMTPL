85c4050224cc94527157086dfaa037f0a5e7b492
Pranav Mahajan <parumahajan3000@gmail.com>
2025-11-27 16:18:01 +0530

    If-Else Statement commit

---
+++ BridgeLabz-Training/Basics/If_Else.cs
@@ -0,0 +1,27 @@
+// Using if-statements to check if the number is less than a specific number or not.
+
+using System;
+namespace DSA
+{
+    public class B1_If_St
+    {
+        public static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+
+            if (a < 20)
+            {
+
+                // STRING CONCATENATION
+                Console.WriteLine("a: " + a);
+
+                // STRING INTERPOLATION
+                Console.WriteLine($"a: {a}");
+
+                // STRING FORMATTING (using placeholder {})
+                Console.WriteLine("a: {0}, b: {1}", a, b);
+            }
+        }
+    }
+}
